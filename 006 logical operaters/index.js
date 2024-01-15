6c39bfe875e3901e5b206b984a5893bb2775e73c
Rasheed Ahmad <118961857+Rasheed-Ahmad07@users.noreply.github.com>
2024-01-15 22:09:20 +0500

    Add files via upload

---
+++ 006 logical operaters/index.js
@@ -0,0 +1,12 @@
+//! logical operaters
+//& result boolean just like comparison operaters
+//* ........ &&    logical and  , || logical or
+// console.log("bilal");
+
+
+// var x=100;
+// var y =3;
+// console.log(x < 6 ||  y < 5);
+
+
+// console.log(true && false);
